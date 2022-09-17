f4052891738101d41239aa4ce78676e6c853ffd7
harshitha <80756064+harshitha04a@users.noreply.github.com>
2022-09-17 11:39:49 +0530

    Update f00d_recog_inception.py

--- f00d_recog_inception.py
+++ f00d_recog_inception.py
@@ -79,12 +79,12 @@ Every one likes food!
 ''')
 def add_bg_from_url():
     st.markdown(
-         f"""
+         """
          <style>
-         p {
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+         body{
+             background-image: url("f23.jpg");
              background-attachment: fixed;
-             background-size: cover
+             background-size: cover;
          }
          </style>
          """,
