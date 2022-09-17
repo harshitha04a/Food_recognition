7e6a51e6d85b72a8b09a7128e13aa3c6e4b760ed
harshitha <80756064+harshitha04a@users.noreply.github.com>
2022-09-17 11:10:54 +0530

    Update f00d_recog_inception.py

--- f00d_recog_inception.py
+++ f00d_recog_inception.py
@@ -75,49 +75,11 @@ ss.title("Food Recognition using inception-V3")
 
 ss.markdown(
 '''
-Every one likes food! This deployment recognizes 11 different classes of food using a SOTA Inception V3 Transfer Learning.\n
+Every one likes food! 
 ''')
 
-ss.image("f1.jpg")
-ss.markdown(
-'''
-### Inception V3
-- The paper for Inception can be found [here](https://arxiv.org/abs/1512.00567v3)\n
 
-- The paper implementation using pytorch can be found [here](https://github.com/pytorch/vision/blob/6db1569c89094cf23f3bc41f79275c45e9fcb3f3/torchvision/models/inception.py#L64)
 
-- Inception-v3 is a convolutional neural network architecture from the Inception family that makes several improvements including using 
-  - Label Smoothing,
-  - Factorized 7 x 7 convolutions,\n 
-  and the use of an auxiliary classifer to propagate label information lower down the network (along with the use of batch normalization for layers in the sidehead).
-- Training on 16,600 images yielded 90% accuracy on train and 76% accuracy on validation. over 50 epochs!
-- This model is saved and used later
-'''
-)
-ss.markdown(
-'''
-### Model Architecture
-''')
-ss.image("inception_2.png")
-
-ss.markdown('### Dataset Details and Classes')
-ss.markdown('Data consists of 1.1GB of 16,600 images of different categories of food.')
-ss.markdown('the categories of food that can be classified are ')
-ss.markdown(
-  '''
-    - Bread
-    - Dairy Product
-    - Dessert
-    - Egg
-    - Fried Food
-    - Meat
-    - Noodles-pasta
-    - Rice
-    - Seafood
-    - Soup
-    - Vegetable-fruit
-  '''
-)
 ss.markdown('Dataset is obtained from [kaggle](https://www.kaggle.com/trolukovich/food11-image-dataset)')
 
 
